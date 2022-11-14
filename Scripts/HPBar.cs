2d85f45dab668e3591f075acc97aa78c789cf824
zzolikoff1027 <78103329+zzolikoff1027@users.noreply.github.com>
2022-11-14 10:50:40 -0700

    Add files via upload

---
+++ Scripts/HPBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+	private Slider slider;
+
+	private void Start()
+	{
+		slider = GetComponentInChildren<Slider>();
+		GetComponentInParent<IHealth>().OnHPPctChanged += HandleHPPctChanged;
+	}
+
+	private void HandleHPPctChanged(float pct)
+	{
+		slider.value = pct;
+	}
+}
---
+++ Scripts/StandardHealth.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class StandardHealth : MonoBehaviour, IHealth
+{
+    [SerializeField] private int startingHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHPPctChanged = delegate { };
+    public event Action OnDied = delegate { };
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public float CurrentHpPct
+    {
+        get { return (float)currentHealth / (float)startingHealth; }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException("Invalid Damage amount specified: " + amount);
+
+        currentHealth -= amount;
+
+        OnHPPctChanged(CurrentHpPct);
+
+        if (CurrentHpPct <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        OnDied();
+        GameObject.Destroy(this.gameObject);
+    }
+}
